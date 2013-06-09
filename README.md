97e7ddd2d9d43981be827190ff7142917b0f7578
Amy Stephen <AmyStephen@gmail.com>
2013-06-09 13:21:29 -0500

    Create gitignore.txt

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 Create.gitignore
 ================
 
-Creates a .gitignore with all files currently in localhost website. To use when creating PHP-based add-ons, like plugins or themes, within an existing core code base and you want only the add-ons to be committed to repository.
+Creates a .gitignore file listing all files currently in localhost website.
+
+For use when creating PHP-based add-ons, like plugins or themes when you want to develop within an existing PHP
+core code base (doesn't matter which, this is a framework-, cms-independent approach) and you want only the
+add-ons you are working to to be committed to repository.
+
+For security purposes, do not use this approach on a production website, only on a localhost site.
+
+ ##To use:
+
+ 1. Install the software you want to use (but not save to your repository) into a localhost environment.
+ 2. Drop the `create.php` file in the root of this website.
+ 3. Open your browser, and navigate to the file within that site: ex. http://localhost/site/create.php
+ 4. Look for the `.gitignore.txt` file on the root of the localhost website.
+ 5. Rename `.gitignore.txt` to `.gitignore`
+
+ ##Trouble-shooting:
+
+ 1. Didn't create a file: check your permissions. You can create the create.php file using an ID with
+ access, then make certain it is enabled for update for everyone. (Again, do not use on a production website.)
---
+++ create.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package   Molajo
+ * @copyright 2013 Amy Stephen. All rights reserved.
+ * @license   http://www.opensource.org/licenses/mit-license.html MIT License
+ */
+
+$ignore = array();
+
+$ignore[] = '.buildpath' . "\n";
+$ignore[] = '.DS_Store' . "\n";
+$ignore[] = '.idea' . "\n";
+$ignore[] = '.project' . "\n";
+$ignore[] = '.settings' . "\n";
+$ignore[] = '.gitignore' . "\n";
+$ignore[] = '.gitattributes' . "\n";
+$ignore[] = 'composer.phar' . "\n";
+$ignore[] = 'composer.lock' . "\n";
+
+$current_path = __DIR__;
+
+$objects = new RecursiveIteratorIterator
+(new RecursiveDirectoryIterator($current_path),
+    RecursiveIteratorIterator::SELF_FIRST);
+
+$dirRead = dir(__DIR__);
+$path    = $dirRead->path;
+while ($entry = $dirRead->read()) {
+    if ($entry == '.' || $entry == '..' || $entry == '.git') {
+
+    } else {
+        if (is_dir($path . '/' . $entry)) {
+        } else {
+            $item     = $path . '/' . $entry;
+            $ignore[] = substr($item, strlen($current_path) + 1, 99999) . "\n";
+        }
+    }
+}
+$dirRead->close();
+
+foreach ($objects as $name => $object) {
+
+    if ($object->isDir()) {
+        if (basename($name) == '.' || basename($name) == '..' || basename($name) == '.git') {
+        } else {
+            $test = substr(substr($name, strlen($current_path) + 1, 9999), 0, 4);
+            if ($test == '.git') {
+            } else {
+                $dirRead = dir($name);
+                $path    = $dirRead->path;
+                while ($entry = $dirRead->read()) {
+                    if (basename($name) == '.' || basename($name) == '..' || basename($name) == '.git') {
+                    } else {
+                        if (is_dir($path . '/' . $entry)) {
+                        } else {
+                            $item     = $path . '/' . $entry;
+                            $ignore[] = substr($item, strlen($current_path) + 1, 99999) . "\n";
+                        }
+                    }
+                }
+                $dirRead->close();
+            }
+        }
+    }
+}
+
+file_put_contents('gitignore.txt', $ignore);
+
